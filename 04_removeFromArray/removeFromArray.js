2e77ff2dd78f13c90e624780a0310a922211e143
MKcode-45 <kalotamishael@gmail.com>
2025-03-18 11:18:09 +0200

    Completed exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+
+    for(let i = 0; i < arr.length; i++){
+        for(let j = 0; j < args.length; j++){
+            arr = arr.filter(()=> arr[i] !== args[j] );
+        }
+    }
+    
+    return arr;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const roundOff = function (val){
+  return Math.round(val*10)/10;
+}
+
+const convertToCelsius = function(degreesInFahrenheit) {
+  let celsiusFormula = (degreesInFahrenheit - 32)* 5/9;
+  return roundOff(celsiusFormula);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreesInCelsius) {
+  let fahrenheitFormula = degreesInCelsius*(9/5) + 32;
+  return roundOff(fahrenheitFormula);
 };
 
 // Do not edit below this line
