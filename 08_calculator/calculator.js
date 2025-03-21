8ec496228d92a58f611b8c255cbef9906557e5cf
MKcode-45 <kalotamishael@gmail.com>
2025-03-21 05:43:14 +0200

    Excerises completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a -b;
 };
 
-const sum = function() {
-	
+const sum = function(n) {
+	return n.reduce((accumulator, currentValue) =>{
+    return accumulator + currentValue ;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(n) {
+	return n.reduce((accumulator, currentValue) =>{
+    return accumulator * currentValue ;
+  });
 };
 
-const power = function() {
-	
+
+const power = function(a,b) {
+  return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(f) {
+	if(f === 0){
+    return 1;
+  }
+
+  let result = 1;
+  for(f; f > 0; f--){
+    result *= f;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
