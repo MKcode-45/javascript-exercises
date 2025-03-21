08882f3359e0ee301f49a84b54e5f37dbac77492
MKcode-45 <kalotamishael@gmail.com>
2025-03-21 10:31:43 +0200

    completed palindromes task

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let reversedStr = str.split("").reverse().filter((letters) => /^[A-Za-z0-9]$/.test(letters)).join('').toLowerCase();
+    let strInput = str.split("").filter((letters) => /^[A-Za-z0-9]$/.test(letters)).join('').toLowerCase();
+    if(strInput === reversedStr) return true;
 
+    return false;
 };
 
 // Do not edit below this line
